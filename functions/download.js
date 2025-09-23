b4e56e6d1ac1d164bbd432f7416ecf51f84fdde4
yonatan <yonatan.lin@outlook.com>
2025-09-23 15:52:43 +0800

    feat: 优化日志打印信息

--- functions/download.js
+++ functions/download.js
@@ -2,79 +2,89 @@ import fs from "fs";
 import archiver from "archiver";
 import config from "../config.js";
 import { logger } from "../middlewares/log.js";
+import dayjs from "dayjs";
+import { filesize } from 'filesize'
+const log = logger();
 
-const cleanUpTempFile = (tempFilePath) => {
-  let level = 'info';
-  let message = [];
+const getAbsolutePath = (url) => {
+  const urlWithoutParams = decodeURIComponent(url).split("?")[0];
+  const node = config.NODE.find((node) =>
+    urlWithoutParams.startsWith(`/${node.name}`)
+  );
+  const absolutePath = urlWithoutParams
+    .replace(`/${node.name}`, node.path)
+    .replace(/\/$/, "");
+  const fileName = absolutePath.split('/').pop() + '.zip';
+  log.info(`[Download] "%s" Requested path: %s, Mapped to: %s`, fileName, urlWithoutParams, absolutePath);
+  return absolutePath;
+}
+
+const createZipFile = (sourceDir) => {
+  return new Promise((resolve, reject) => {
+    const fileName = sourceDir.split('/').pop() + '.zip';
+    log.info(`[Download] "%s" Downloading directory: %s, Creating zip file...`, fileName, sourceDir);
+
+    const tempDir = `/tmp/FIndex/${dayjs().format('YYYYMMDD')}/`;
+    fs.mkdirSync(tempDir, { recursive: true });
+
+    const outPath = `${tempDir}${fileName}`;
+    const archive = fs.createWriteStream(outPath);
+    const zip = archiver("zip", { zlib: { level: 9 } });
+
+    archive.on("error", (err) => {
+      reject(err);
+    });
+
+    zip.pipe(archive);
+    zip.directory(sourceDir, false);
+    zip.finalize();
+
+    archive.on("close", () => {
+      const stats = fs.statSync(outPath);
+      log.info(`[Download] "%s" Created successfully. OutPath: %s, Size: %s`, fileName, outPath, filesize(stats.size));
+      resolve(outPath);
+    });
+  });
+};
+
+const cleanUpZipFile = (tempFilePath) => {
+  const fileName = tempFilePath.split('/').pop();
   try {
     fs.unlinkSync(tempFilePath);
-    message = ["[Download Zip] Cleaned up temporary file: %s", tempFilePath];
+    log.info(`[Download] "%s" Cleaned up temporary file`, fileName);
   } catch (error) {
-    level = 'error';
-    message = ["[Download Zip] Failed to clean up temporary file: %s", tempFilePath, error];
+    log.error(`[Download] "%s" Failed to clean up temporary file:\n %s`, fileName, error);
   }
-  return [level, message];
 }
 
 export default async (req, res, next) => {
   if (req.query.download === undefined) return next();
 
-  const log = logger(req);
-  const handleCleanup = () => {
-    const [level, message] = cleanUpTempFile(zipPath);
-    log[level](...message);
-  };
-  
   let absolutePath = "";
-  let zipPath = "";
+  let tempZipFilePath = "";
   try {
-    const reqPath = decodeURIComponent(req.baseUrl + req.path);
-    const node = config.NODE.find((node) =>
-      reqPath.startsWith(`/${node.name}`)
-    );
-
-    absolutePath = reqPath
-      .replace(`/${node.name}`, node.path)
-      .replace(/\/$/, "");
-    // console.log(
-    //   "[Download] Requested path: %s, Mapped to: %s",
-    //   reqPath,
-    //   absolutePath
-    // );
+    const absolutePath = getAbsolutePath(req.originalUrl);
 
     if (fs.statSync(absolutePath).isFile()) return next();
 
-    log.info("[Download Zip] Creating zip for directory: %s", absolutePath);
-    zipPath = `${absolutePath}.zip`;
-    const archive = fs.createWriteStream(zipPath);
-    const zip = archiver("zip", { zlib: { level: 9 } });
+    tempZipFilePath = await createZipFile(absolutePath);
 
-    archive.on("error", (err) => {
-      throw err;
-    });
+    res.download(tempZipFilePath, (err) => {
+      const fileName = tempZipFilePath.split('/').pop();
+      if (err) {
+        log.error(`[Download] "%s" Error sending file:\n %s`, fileName, err);
+        cleanUpZipFile(tempZipFilePath);
+        return res.status(500).send("处理失败");
+      }
 
-    zip.pipe(archive);
-    zip.directory(absolutePath, false);
-    zip.finalize();
-
-    archive.on("close", () => {
-      log.info("[Download Zip] Zip created successfully: %s", zipPath);
-      res.download(zipPath, (err) => {
-        if (err) {
-          log.error("[Download Zip] Error sending file: %s", zipPath, err);
-          handleCleanup();
-          return res.status(500).send("处理失败");
-        }
-
-        log.info("[Download Zip] File sent successfully: %s", zipPath);
-        handleCleanup();
-      });
+      log.info(`[Download] "%s" Successfully sent file`, fileName, tempZipFilePath);
+      cleanUpZipFile(tempZipFilePath);
     });
   } catch (err) {
-    log.error("[Download Zip] Error processing %s:", absolutePath, err);
+    log.error(`[Download] Error processing: %s\n %s`, absolutePath, err);
     // 统一清理临时文件
-    if (zipPath && fs.existsSync(zipPath)) {
-      handleCleanup();
+    if (tempZipFilePath && fs.existsSync(tempZipFilePath)) {
+      cleanUpZipFile(tempZipFilePath);
     }
     res.status(500).send("处理失败");
   }
--- middlewares/log.js
+++ middlewares/log.js
@@ -1,23 +1,26 @@
 import dayjs from "dayjs";
 import config from "../config.js";
 import { checkAuth } from "./auth.js";
+let _req = {};
 
-export const logger = (req) => {
+export const logger = () => {
   const date = dayjs().format("YYYY-MM-DD HH:mm:ss");
 
   return {
     info: (...args) => {
       const arg0 = args.shift();
-      console.log(`${req.ip} -- [${date}] | INFO | ${arg0}`, ...args);
+      console.log(`${_req.ip} -- [${date}] | INFO  | ${arg0}`, ...args);
     },
     error: (...args) => {
       const arg0 = args.shift();
-      console.error(`${req.ip} -- [${date}] | ERROR | ${arg0}}`, ...args);
+      console.error(`${_req.ip} -- [${date}] | ERROR | ${arg0}}`, ...args);
     },
   }
 }
 
 export default async (req, res, next) => {
+  _req = req;
+
   const filterRules = [
     '/favicon.ico',
     '/public/',
@@ -29,7 +32,7 @@ export default async (req, res, next) => {
     const duration = Date.now() - start;
     const time = dayjs(start).format('YYYY-MM-DD HH:mm:ss');
     const httpStatus = res.statusCode;
-    const isFiltered = filterRules.some(rule => req.originalUrl.startsWith(rule) && httpStatus === 304);
+    const isFiltered = filterRules.some(rule => req.originalUrl.startsWith(rule) && (httpStatus === 304 || httpStatus === 200));
     const authStatus = checkAuth(req);
 
     if (!isFiltered) {
