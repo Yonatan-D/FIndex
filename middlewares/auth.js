fec22a659deb767898a788dff20d98eb118a8d13
yonatan <yonatan.lin@outlook.com>
2025-09-25 16:03:30 +0800

    fix: 未开启token校验应该在最前

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,16 +3,16 @@ import config from "../config.js";
 export const checkAuth = (req) => {
   const allowHost = config.IP_WHITE_LIST.includes(req.hostname);
   const allowToken = req.query?.token === config.TOKEN || req.headers.cookie?.includes(`x-token=${config.TOKEN}`);
-  if (allowHost) {
-    return {
-      status: true,
-      message: "whitelist",
-    };
-  }
   if (!config.TOKEN) {
     return {
       status: true,
       message: "no-token-set",
+    };
+  }
+  if (allowHost) {
+    return {
+      status: true,
+      message: "whitelist",
     };
   }
   if (allowToken) {
