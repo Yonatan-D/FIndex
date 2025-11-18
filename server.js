93dc7007f62651d463a3141db1b925c77d17c581
yonatan <yonatan.lin@outlook.com>
2025-11-18 14:20:04 +0800

    refactor: 移动核心文件到lib目录并优化路由配置

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import 'dotenv/config';
 import config from './config.js';
 import log from './middlewares/log.js';
 import auth from './middlewares/auth.js';
-import createBucketRoutes from './functions/createBucketRoutes.js';
+import createBucketRoutes from './lib/routes.js';
 const { PORT, PREFIX } = config;
 
 const app = express();
@@ -14,7 +14,7 @@ app.set('x-powered-by', false);
 app.use(compression());
 app.use(log);
 app.use(auth);
-app.use(PREFIX, createBucketRoutes());
+app.use(PREFIX, createBucketRoutes({ prefix: PREFIX }));
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(c.green(`Starting at http://localhost:${PORT}${PREFIX}`));
